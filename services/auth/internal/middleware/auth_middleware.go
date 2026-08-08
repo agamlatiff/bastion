8db1bd5d7a07429ef96294255f453cfebab79539
Agam Latifullah <agam.latiff@gmail.com>
2026-08-09 05:15:56 +0700

    feat: implement auth middleware for token validation and add journal directory to gitignore

---
+++ services/auth/internal/middleware/auth_middleware.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/agamlatiff/bastion/services/auth/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(authService service.AuthService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get Header Authorization
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") { 
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Missing or invalid authorization header",
+			})
+			return
+		}
+
+		// Get Token Authorization
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		
+		// Validation token & check redis blacklist via AuthService
+		user, err := authService.ValidateToken(c.Request.Context(), tokenStr)
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error" : err.Error(),
+			})
+			return
+		} 
+
+		// Save user data & raw token into Context Gin 
+		c.Set("currentUser", user)
+		c.Set("token", tokenStr)
+
+		// Next Handler 
+		c.Next()
+	}
+}
